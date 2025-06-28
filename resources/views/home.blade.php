b5c5f89d34ac6fb0657b3313053d29f6f3844d91
John Andre Yap <johnandresyap510@gmail.com>
2025-06-28 14:30:42 +0800

    Tested things out

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>This is a test</p>
 </body>
 </html>
